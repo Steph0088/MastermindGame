01de64fff470dd13132f1efe978b17f0df94de51
steph0088 <15236701+Steph0088@users.noreply.github.com>
2022-05-17 22:27:31 -0700

    Added classes

---
+++ specs/test_helper.rb
@@ -0,0 +1,21 @@
+require 'simplecov'
+SimpleCov.start do
+    add_filter 'test/'
+end
+
+
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+
+#require 'date'
+
+#require_relative '../lib/date_range'
+# require_relative '../lib/reservation'
+# require_relative '../lib/room_block'
+# require_relative '../lib/hotel'
