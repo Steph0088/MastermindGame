ddd447789a3eb344e0517ca33450f877ba7e8de7
steph0088 <15236701+Steph0088@users.noreply.github.com>
2022-05-20 01:35:26 -0700

    Added Hints class and corresponding tests

--- specs/hints_tests.rb
+++ specs/hints_tests.rb
@@ -4,15 +4,20 @@ require_relative "test_helper"
 describe "Hints class" do
     describe "Given input of colors returns hints " do
         hints = Hints.new
-        hints.give_hints(["red","black","white","red"])
-        it "returns a hash with the keys being colors and the value as the corresponding hint" do
-          expect("red").must_equal "Correct Position and Correct Color"
-          expect("white").must_equal "Correct Color but incorrect Position"
-          expect("black").must_equal "Incorrect Color and Position"
+        round_hints = hints.give_hints(["red","black","white","red"])
+        
+        it "returns a array with the keys being colors and the value as the corresponding hint" do
+          expect(round_hints).must_include "Correct Position and Correct Color"
+          expect(round_hints).must_include "Correct Color but Incorrect Position"
+          expect(round_hints).must_include "Incorrect Color and Position"
         end
 
-        it "" do
-             "not yet implemented"
+        it "Adds round hints to total hints" do
+             round_hints = ["Correct Position and Correct Color", "Incorrect Color and Position",  "Incorrect Color and Position","Correct Color but Incorrect Position" ]
+             hints.add_to_total_rounds_hints(round_hints)
+             total = hints.get_total_round_hints()
+             print total
+             expect(total[0]).must_equal round_hints
         end
     end
 end
