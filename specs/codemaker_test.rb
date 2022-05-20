ef2f176782444bcc1a8ec135351ae3c10ce9cfaa
steph0088 <15236701+Steph0088@users.noreply.github.com>
2022-05-19 19:48:44 -0700

    added tests for codemaker class and initial functionality to codemaker class

---
+++ specs/codemaker_test.rb
@@ -0,0 +1,30 @@
+require_relative "test_helper"
+
+
+describe "Codemaker class" do
+    sequence = []
+    client = RandomSequenceClient.new()
+    sequence = client.get_random_sequence("easy")
+
+    describe "get_random_sequence and covert to color coded sequence" do
+        codemaker = CodeMaker.new()
+        converted_sequence = codemaker.convert_to_colors(sequence)
+        it "returns an array of strings" do
+          expect(converted_sequence).must_be_instance_of Array
+          expect(converted_sequence[0]).must_be_instance_of String
+        end
+
+        it "" do
+             "not yet implemented"
+        end
+    end
+
+    describe "" do
+        "not yet implemented"
+    end
+
+    describe "" do
+         "not yet implemented"
+    end
+end
+    
