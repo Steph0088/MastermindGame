38747f960a232a67e4bf19edde4147ca3825f4ef
steph0088 <15236701+Steph0088@users.noreply.github.com>
2022-05-23 16:34:23 -0700

    Recent Bug Fixes, sill more fixes needed

--- Controller/game.rb
+++ Controller/game.rb
@@ -5,42 +5,51 @@ require_relative "/Users/fridakahlo/Reach/MastermindGame/Model/hints.rb"
 require_relative "/Users/fridakahlo/Reach/MastermindGame/Model/codebreaker.rb"
 require_relative "/Users/fridakahlo/Reach/MastermindGame/Model/IOManager.rb"
 
-class Game
-    attr_accessor :active, :isPlaying, :max_rounds, :current_round, 
-    :current_guess, :total_rounds_guesses, :code_broken
 
-    def  initialize()
+class Game
+    attr_accessor :number_of_rounds, :code_broken, :current_round, :active, :is_playing
+
+    COlORS_AVAILABLE = {"purple" => 0, "red"=> 1, "blue"=> 2, "green"=> 3, "orange" => 4, "white"=> 5, "pink"=> 6, "teal"=> 7, "black"=> 8, "magenta"=> 9}
+    def initialize()
         @io = IOManager.new
         @random_code = RandomSequenceClient.new
         @codemaker = CodeMaker.new
         @hints = Hints.new
-        @max_rounds = 10
+        @current_guess = []
+        codebreaker = Codebreaker.new()
+
         @active = false
-        @isPlaying = false
-        @total_rounds_guesses = []
-        @code_to_decipher = []
+        @is_playing = false
         @code_broken = false
+        @number_of_rounds = 0
+        @code_to_decipher = []
+        @total_rounds_guesses = []
+        @total_rounds_hints = []
+        @history_display = {}
     end
 
-    # assign player name, give user the rules of the game, set player to active
-    def init()
-        response = @io.prompt_player("Welcome To Mastermind!\nDecipher the colors and their correct positions in the secret code to win this game.\n Each code is made up of 4 colors with a total of 10 Rounds.\n Would you like to play?\n Y/N")
+    def start()
+        @is_playing = true
+    end
+
+    def initiate_game()
+        response = @io.prompt_player("Welcome To Mastermind!\nDecipher the colors and their correct positions in the secret code to win this game.\n Each code is made up of 4 colors with a total of 10 Rounds.\n Would you like to play?\n Y/N", false)
         response = response.downcase
-        if (response == "y" || response == "yes")
-            player_name = @io.prompt_player("What is your name?")
-            codebreaker = Codebreaker.new(player_name)
+        if response == "y" || response == "yes"
+            start()
             @active = true
-            @io.print_line("Hello #{player_name}, you are now a codebreaker.Good Luck!", true)
-            @io.print_line("Press any key to begin the game",true)
-            @io.continue_to_game()
+            #@io.print_line("Press any key to begin the game",true)
+            #@io.continue_to_game()
         end
         return @active
     end
-   
-    #Once you start game keep track of rounds so they don't exceed 10
-    #Once you start game keey track of codebreaker_Score, ComputerScore
-    def start()
-        isPlaying = true
+
+    def game_rules()
+        @io.clear_console()
+        @io.print_line("The Code to be deciphered:", false)
+        @io.print_line("[X,X,X,X]",false)
+        @io.print_line("Possible Colors in Code: purple, red, blue, green, orange, white, pink, magenta, teal", false)
+        @io.print_line("------------------------------------------------------------------------------------------", false)
     end
 
     def generate_code()
@@ -48,92 +57,86 @@ class Game
         @code_to_decipher = @codemaker.convert_to_colors(sequence)
     end
 
-    def display_conceiled_code()
-        @io.clear_console()
-        @io.print_line("The Code to be deciphered:", false)
-        @io.print_line("[X,X,X,X]",false)
-        @io.print_line("Possible Colors in Code: purple, red, blue, green, orange, white, pink, magenta, teal", false)
-    end
-
-    def prompt_guess()
-        guess = @io.prompt_player("Please enter 4 colors separated by commas, order matters:")
-        #I would consider adding some validation here rather than just taking the first character of the input
-        #TODO validate that input is strings
-        #TODO validates that they chose the correct code
+    def prompt_codebreaker()
+        @current_guess = @io.prompt_player("Please enter 4 colors separated by commas:")
+        #Check to see if guess matches colors in dictionary
         
-        #might have to use regex here to validate the input
-
-        return guess.downcase
+        @current_guess = @current_guess.split(',')
+        @current_guess.each do |color|
+            color.downcase!
+        end
+        
+        if @current_guess.length != 4
+            @io.print_line("Check the number of colors")
+            return false
+        end
+        @current_guess.each do |color|
+            if !COlORS_AVAILABLE[color]
+                @io.print_line("Incorrect color input: #{color} is not an available color to choose from check reference above, check spelling", true) 
+                return false
+            end
+            return true
+        end
     end
 
-    def handle_guess(guess)
-        current_round = Round.new(guess,@code_to_decipher)
+    def handle_guess()
+        @current_round = Round.new(@current_guess, @code_to_decipher)
         @total_rounds_guesses.push(current_round.user_guess)
-
-        if (@max_rounds != 0)
-            #check the guess against the code to retrieve the colors
-            results_of_guess = current_round.check_guess_against_code()
+        #check the guess against the code to retrieve the colors
+        results_of_guess = current_round.check_guess_against_code_for_matching_values()
             if results_of_guess == ["red","red","red","red"]
                 @code_broken = true
+                @is_playing = false
+                @active = false
             end
-            #add the current guesses hints to hints history so it can be displayed to user.
-            current_guess_hints = @hints.give_hints(results_of_guess)
-            @io.print_line(current_guess_hints, false)
-            @io.print_line(@total_rounds_guesses,false)
+        current_guess_hints = @hints.give_hints(results_of_guess)
 
-            hints_history = @hints.add_to_total_rounds_hints(current_guess_hints)
-            @max_rounds -= 1
-        else
-            if @code_broken == true
-                game_won()
-            else
-                game_lost() 
-            end
-        end
-
+        @total_rounds_guesses.push(@current_guess)
+        @total_rounds_hints.push(current_guess_hints)
+        @number_of_rounds += 1
+        self.update_guess_and_hint_history(@current_round.user_guess, current_guess_hints)
     end
 
-    def display_guesses_and_hints_history()
-        @io.print_line(@total_rounds_guesses)
-        @io.print_line(@total_rounds_hints)
+    def display()
+        return @io.print_line(@history_display, false)
     end
-
-   
     def game_lost()
         @io.print_line("YOU LOSE! \n Secret Code Revealed: #{@code_to_decipher}", true)
-        @isPlaying = false
+        @is_playing = false
+        @active = false
     end
 
     def game_won()
-        @io.print_line("YOU WON! CODE BROKEN \nYou are a MASTERMIND!! Code: #{total_rounds_guesses.last()}", true)
-        @isPlaying = false
+        @io.print_line("YOU WON! CODE BROKEN \nYou are a MASTERMIND!! Winning Code: #{@total_rounds_guesses.last()}", true)
+        @is_playing = false
+        @active = false
     end
 
     def prompt_new_game()
         restartResponse = @io.prompt_player("Nice game! Would you like to play again? Y/N")
         restartResponse.downcase()
         if (restartResponse == "y" || restartResponse == "yes")
-            Start()
+            @active = true
+            @is_playing = true
+            return true
         else
-            active = false
+            @active = false
+            return false
         end
     end
 
     def finish()
         @io.prompt_player("Game Over.\n We shall meet again.", true)
     end
+   
+    def update_guess_and_hint_history(guess, current_guess_hints)
+        last_guess_made  = @total_rounds_guesses.last()
+        last_guess_string = ""
+        last_guess_made.each do |color|
+            last_guess_string << " #{color}"
+        end
+        
+        last_round_hints = @total_rounds_hints.last()
+        @history_display["Round Number:#{number_of_rounds} Guess: #{last_guess_string}"] = last_round_hints 
+    end
 end
-
-
-game = Game.new
-
-
-#game.generate_code()
-#game.display_conceiled_code()
-#game.handle_guess(["red","purple","teal","white"])
-#game.game_lost()
-#game.game_won()
-#game.prompt_new_game()
-#game.finish()
-#game.display_guesses_and_hints_history()
-
